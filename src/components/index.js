475ea25d253e969df981180a040718526c9e49da
Dssaenz <darwinsaenz105@gmail.com>
2022-11-16 21:00:46 -0500

    added functionality to get fastest ships

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import FetchFind from './FetchFind';
 import FetchOrder from './FetchOrder';
 import FetchCounter from './FetchCounter';
+import FastestShip from './FastestShip/FastestShip';
 
-export { FetchFind, FetchOrder, FetchCounter };
+export { FetchFind, FetchOrder, FetchCounter, FastestShip };
